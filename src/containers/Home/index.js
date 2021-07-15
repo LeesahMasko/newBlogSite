7ed56b8e69366c60de7cda44ad0cd03097df4ae9
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2021-07-15 14:37:51 -0700

    adding new home page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -4,11 +4,27 @@ import './styles.css';
 
 const Home = props=> {
     return(
-        <div>
-            Home
+        <div className="homeContainer">
+            <div className="welcomeContent">
+                I believe that life is a great balancing act.  We are all striving to find that perfect balance where we find true peace, happiness and confidence in who we are.
+                <p>This site isn't going to contain anything ground breaking, it will be a collections of worthwile physical and mental exercise as well as advice and guidance for nutrition.</p>
+            </div>
+            <div className="tripleBoxes">
+                <div className="leftComponent">left</div>
+                <div className="middleComponent">
+                    middle
+                </div>
+                <div className="rightComponent">
+                    right
+                </div>
+            </div>
+
         </div>
 
     )
 }
 
 export default Home;
+
+
+
--- src/data/KitchenPosts/kitPost_1.js
+++ src/data/KitchenPosts/kitPost_1.js
@@ -11,29 +11,29 @@ function KitPost_1() {
       <h1 className="postTitle">Learn from my mistakes</h1>
       <span className="postByandDate"> July 15, 2021</span>
       </div>
-      <div className="postImageContainerRight">
-          <div className="textOnLeft">The weight crept on so slowly that I didn't really even notice it.  When I was about 35 I had one of those eye opening moments when I saw myself for what seemed like the first time.  I was in Switzerland and I had set off on a hike.  At some point, I set up the self-timer on my camera to snap a picture of myself along this lovely trail. When I downloaded the pictures, I saw my bulkyness is full view!  I think I hadn't really noticed it before, but there it was.  After that trip I decided it was time to make some changes.</div>
+      <div className="postImageContainer">
+          {/* <div className="textOnLeft">The weight crept on so slowly that I didn't really even notice it.  When I was about 35 I had one of those eye opening moments when I saw myself for what seemed like the first time.  I was in Switzerland and I had set off on a hike.  At some point, I set up the self-timer on my camera to snap a picture of myself along this lovely trail. When I downloaded the pictures, I saw my bulkyness is full view!  I think I hadn't really noticed it before, but there it was.  After that trip I decided it was time to make some changes.</div> */}
 
           <img className="kitchenImage" src='../../../BlogPostImages/kitchen.jpg' alt="cooking in the kitchen"/>
       </div>
       <div className="postContent">
-      <p>I don't think there is any perfect way to eat...</p>
+      <p>I don't think there is any one perfect way to eat...</p>
       <p>I wish there was.</p>
       <p>My goal here is to give you the tips, recipes and information that actually helped me.
       </p>
 
-      <h3>A synopsis of my health and fitness path</h3>
-      <p>I was always scrawny growing up.  My family didn't eat healthy, per se... we ate frugally.  There were 6 kids in my family so dinners were often filled with the cheapest hamburger meat available.  We drank milk, ate bread, our cheese was always orange and pancakes were a staple.</p>
+      <h3>Facing the facts.  Things had slowly gotten out of hand.</h3>
+      <p>I was always scrawny growing up.  My family didn't eat unhealthy, per se... we ate frugally.  There were 6 kids in my family so dinners were often filled with the cheapest hamburger meat available.  We drank milk, ate bread, our cheese was always orange & came in giant bag from Costco, and pancakes were a staple.</p>
       <p>We always had a vegetable at dinner.  Most the time there were prepared in such way that made them increasingly more unappetising than if you ate them just picked from the garden with dirt and bugs. (Think zucchini cooked in a pan with butter until it became green glop)</p>
       <p>I was pretty skinny until I got to about 25 years old, then I slowly started to again some extra padding all over.</p>
-      <p>The weight crept on so slowly that I didn't really even notice it.  When I was about 35 I had one of those eye opening moments when I saw myself for what seemed like the first time.  I was in Switzerland and I had set off on a hike.  At some point, I set up the self-timer on my camera to snap a picture of myself along this lovely trail. When I downloaded the pictures, I saw my bulkyness is full view!  I think I hadn't really noticed it before, but there it was.  After that trip I decided it was time to make some changes.</p>
+      <p>The weight crept on so slowly that I didn't really even notice it.  When I was 32 I had one of those eye opening moments when I saw myself for what seemed like the first time.  I was in Switzerland and I had set off on a hike.  At some point, I set up the self-timer on my camera to snap a picture of myself along this lovely trail. When I downloaded the pictures, I saw my bulkyness is full view!  I think I hadn't really noticed it before, but there it was.  After that trip I decided it was time to make some changes.</p>
       <div className="postImageContainer">
           <img className="kitchenImage" src='../../../BlogPostImages/SwissHike.jpg' alt="cooking in the kitchen"/>
       </div>
       <p>The first obligatory step... facing the scale.  I needed to see what my starting point was.</p>
       <p>181 pounds.  A shocking number to read.</p>
-      <p>I calculated my BMI: 30.1 Then I looked at the BMI Categories..."Obesity = BMI of 30 or greater"</p>
-      <p>I was considered to by obese. Oof!</p>
+      <p>I calculated my BMI: 30.1 Then I looked at the BMI Categories..."Obesity = BMI of 30 or greater" I was considered to by obese. Oof!</p>
+      <p></p>
       </div>
 
 
