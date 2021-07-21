99a03e52b76511c3c08a1076323e8afde62b37fc
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2021-07-21 16:25:28 -0700

    commiting chages from today

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -6,13 +6,14 @@ const Home = props=> {
     return(
         <div className="homeContainer">
             <div className="welcomeContent">
-                I believe that life is a great balancing act.  We are all striving to find that perfect balance where we find true peace, happiness and confidence in who we are.
+                Life is a great balancing act.  We are all striving to find that perfect balance where we find true peace, happiness and confidence in who we are.
                 <p>This site isn't going to contain anything ground breaking, it will be a collections of worthwile physical and mental exercise as well as advice and guidance for nutrition.</p>
             </div>
             <div className="tripleBoxes">
                 <div className="leftComponent">
                     <p>Food can be magical!</p>
                     <p>It can make us feel euphoric.</p>
+                    <p>It can also be the avenue for some </p>
                 </div>
                 <div className="middleComponent">
                     Our physcal fitness is something that shouldn't be ignored.  Even thought we often do.  Having a goal to work towards is a good way to start.
