eca8bf999c9088fdbd096194e2583c2c4fb89e4f
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2021-07-27 16:29:38 -0700

    making sidebar dynamic for each path continued

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './styles.css'
+import Navbar from '../Navbar'
+import SideBar_Fit from '../../data/SideBar_Posts/sideBar_Fit'
+import SideBar_Kit from '../../data/SideBar_Posts/sideBar_Kit'
+import SideBar_Men from '../../data/SideBar_Posts/sideBar_Men'
 
 /**
 * @author
@@ -7,13 +11,25 @@ import './styles.css'
 **/
 
 const SideBar = (props) => {
+    function whichSideBar() {
+        if NavLink="/kitchen-posts":
+        whichSideBar=SideBar_Kit;
+
+        if NavLink ="/mental-posts":
+        whichSideBar=SideBar_Men;
+
+        if NavLink ="/fitness-posts":
+        whichSideBar=SideBar_Fit;
+
+
+    }
   return(
-//    data -> sidebar_Posts
-    <div className="sideBars" {...props}>
-      {props.children}
-    </div>
+
    )
 
  }
 
 export default SideBar
+
+
+//check to see which tab is active.  depending on what tab is active, have the associated sidebar display
